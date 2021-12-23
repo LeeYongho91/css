39d491e704a5cd229c8ff223fa025f216b2117ed
Essence <skyks4848@naver.com>
2021-12-23 19:59:17 +0900

    20211223

--- flex/반응형 웹사이트/custom.js
+++ flex/반응형 웹사이트/custom.js
@@ -1,5 +1,16 @@
 $(function() {
 
+  // Trigger 
+
+  $('.trigger').click(function() {
+    $(this).toggleClass('active');
+    $('.gnb').toggleClass('active');
+  });
+  $('.gnb a').click(function() {
+    $('.gnb, .trigger').removeClass('active');
+  });
+  
+
   //Sliding jQuery
   $('.gnb a').click(function(e){
     $.scrollTo(this.hash || 0, 800);
